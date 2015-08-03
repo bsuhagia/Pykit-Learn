9e493c5ab1e44108bbf6dbd40c82e7ac3ae756d6
bsuhagia3 <bsuhagia3@gatech.edu>
2015-08-03 16:35:31 -0400

    Added packageToInstall

---
+++ packagesToInstall.py
@@ -0,0 +1,17 @@
+import sklearn
+import statsmodels
+
+import matplotlib
+import seaborn
+import bokeh
+import ggplot
+
+import numpy
+import scipy
+import pandas
+
+import rpy2
+
+import PyQt4
+
+print("Everything works!!")
