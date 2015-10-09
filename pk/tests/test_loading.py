cd3df9a99b4bf652c16a42749c10874e4b90ef08
bsuhagia3 <bsuhagia3@gatech.edu>
2015-10-09 15:36:53 -0400

    Changed read binaries to read only

--- pk/tests/test_loading.py
+++ pk/tests/test_loading.py
@@ -13,7 +13,7 @@ __DIR_NAME = os.path.abspath(os.path.dirname(__file__)) + '/'
 
 def test_load_arff():
     X, y = load_arff(__DIR_NAME + "ratings_best.arff")
-    X2, y2 = cPickle.load(open(__DIR_NAME + 'correct_array.pkl','rb'))
+    X2, y2 = cPickle.load(open(__DIR_NAME + 'correct_array.pkl','r'))
     assert_true((X == X2).all())
     assert_true((y == y2).all())
 
