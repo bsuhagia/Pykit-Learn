2f1db67785c7d347e0710472379ea9685a97a39c
Sean Dai <sdai@gatech.edu>
2015-10-26 18:26:15 -0400

    Improved visualization performance by saving plots as images. Progress bar functionality added. V2

---
+++ pk/utils/prygress.py
@@ -0,0 +1,30 @@
+import sys
+import time
+import threading
+from functools import wraps
+
+def progress(function=None, stream=sys.stdout, char='.', pause=0.2):
+    """Shows a progress bar while a function runs."""
+    if function is None:
+        return lambda func: progress(func, stream, char, pause)
+
+    @wraps(function)
+    def wrap_function(*args, **kwargs):
+        stop = False
+
+        def progress_bar():
+            stream.write('')
+            while not stop:
+                stream.write(char)
+                stream.flush()
+                time.sleep(pause)
+            stream.flush()
+        
+        try:
+            p = threading.Thread(target=progress_bar)
+            p.start()
+            return function(*args, **kwargs)
+        finally:
+            stop = True
+
+    return wrap_function
